acc2d20dcbe58ff47dc20d4cfd4db39a8bcc2b61
Denise Andrade <87955006+Denise73@users.noreply.github.com>
2021-12-21 16:04:58 -0300

    Projeto Final

---
+++ Projeto Final Dez2021/Javascript/Contato.js
@@ -0,0 +1,10 @@
+function enviarMensagem(){
+    const inputNome = document.querySelector("#input_nome");
+    const inputMensagem = document.querySelector("#input_mensagem");
+
+}  
+
+function coracaoBlog(elemento){
+    elemento.classList.toggle('far')
+    elemento.classList.toggle('fas')
+}
